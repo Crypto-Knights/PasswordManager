bd9e6103aaaa2ef66147bc519295b9889613034d
Tabalbar <35385849+Tabalbar@users.noreply.github.com>
2020-06-05 22:20:27 -1000

    hashed passwords in database

--- app/src/api/Routes/users.js
+++ app/src/api/Routes/users.js
@@ -1,5 +1,7 @@
 const router = require('express').Router();
 let User = require('../Model/user.model');
+const jwt = require('jsonwebtoken')
+const bcrypt = require('bcrypt')
 
 router.route('/').get((req,res) => {
     User.find()
@@ -7,24 +9,54 @@ router.route('/').get((req,res) => {
         .catch(err => res.status(400).json('Error: ' + err));
 });
 
+router.route('/login').post((req,res) => {
+    //Authenticate User
+    const email = req.body.email
+    const accessToken = jwt.sign(email, process.env.ACCESS_TOKEN_SECRET)
+    res.json({accessToken: accessToken})
+})
+
+//todo follow steps on https://www.youtube.com/watch?v=mbsmsi7l3r4 - 10:48
+function authenticateToken(req,res,nex) {
+
+}
+
 router.route('/:id').get((req,res) => {
     User.findById(req.params.id)
         .then(users => res.json(users))
         .catch(err => res.status(400).json('Error: ' + err));
 });
 
-router.route('/add').post((req,res) => {
-    const email = req.body.email;
-    const password = req.body.password;
-    const firstName = req.body.firstName;
-    const lastName = req.body.lastName;
-    const questionOne = req.body.questionOne;
-    const answerOne = req.body.answerOne;
-    const newUser = new User({email, password, firstName, lastName, questionOne, answerOne});
+router.route('/add').post(async (req,res) => {
+    try {
+        const salt = await bcrypt.genSalt();
+        const hashedPassword = await bcrypt.hash(req.body.password, salt);
+        const email = req.body.email
+        const password = hashedPassword
+        const firstName = req.body.firstName
+        const lastName = req.body.lastName
+        const questionOne = req.body.questionOne
+        const answerOne = req.body.answerOne
 
-    newUser.save()
+        const newUser = new User ({
+            email,
+            password,
+            firstName,
+            lastName,
+            questionOne,
+            answerOne
+        })
+        newUser.save()
         .then(() => res.json('User added!'))
         .catch(err => res.status(400).json('Error: ' + err));
+        // User.push(newUser)
+
+    } catch {
+        res.status(500).json()
+    }
+
+
+
 });
 
 
